b3b1c6167eac58cc5733114ad8715092acb54263
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-07-11 18:48:12 -0300

    Add overlay for the form

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 /* Selectors */
-const cardsContainer = document.querySelector(".cards-container");
-const bookForm = document.querySelector("#add-book-form");
-const errorMsg = document.querySelector(".error");
-const addBookBtn = document.querySelector(".add-book-btn");
-const formModal = document.querySelector(".form-modal");
+const cardsContainer = document.querySelector('.cards-container');
+const bookForm = document.querySelector('#add-book-form');
+const errorMsg = document.querySelector('.error');
+const addBookBtn = document.querySelector('.add-book-btn');
+const formModal = document.querySelector('.form-modal');
+const overlay = document.querySelector('.overlay');
 
 const library = [];
 
@@ -15,9 +16,7 @@ function Book(title, author, nrPages, isRead) {
 }
 
 function isInLibrary(bookToAdd) {
-	return library.some(
-		(bookInLibrary) => bookInLibrary.title === bookToAdd.title
-	);
+	return library.some((bookInLibrary) => bookInLibrary.title === bookToAdd.title);
 }
 
 function displayBook(book) {
@@ -25,47 +24,47 @@ function displayBook(book) {
 }
 
 function createBookHTML(book) {
-	const container = document.createElement("div");
-	container.classList.add("card");
+	const container = document.createElement('div');
+	container.classList.add('card');
 
-	const cardTitle = document.createElement("h2");
-	cardTitle.textContent = "Title";
+	const cardTitle = document.createElement('h2');
+	cardTitle.textContent = 'Title';
 
-	const title = document.createElement("p");
+	const title = document.createElement('p');
 	title.textContent = book.title;
-	title.classList.add("book-title");
+	title.classList.add('book-title');
 	container.appendChild(cardTitle);
 	container.appendChild(title);
 
-	const cardAuthor = document.createElement("h2");
-	cardAuthor.textContent = "Author";
-	const author = document.createElement("p");
+	const cardAuthor = document.createElement('h2');
+	cardAuthor.textContent = 'Author';
+	const author = document.createElement('p');
 	author.textContent = book.author;
 	container.appendChild(cardAuthor);
 	container.appendChild(author);
 
-	const cardNrPages = document.createElement("h2");
-	cardNrPages.textContent = "Pages";
-	const nrPages = document.createElement("p");
+	const cardNrPages = document.createElement('h2');
+	cardNrPages.textContent = 'Pages';
+	const nrPages = document.createElement('p');
 	nrPages.textContent = book.nrPages;
 	container.appendChild(cardNrPages);
 	container.appendChild(nrPages);
 
-	const readBtn = document.createElement("div");
-	readBtn.classList.add("read");
-	readBtn.addEventListener("click", toggleReadBtn);
-	if (book.isRead === "true") {
-		readBtn.textContent = "Read";
-		readBtn.classList.add("true");
+	const readBtn = document.createElement('div');
+	readBtn.classList.add('read');
+	readBtn.addEventListener('click', toggleReadBtn);
+	if (book.isRead === 'true') {
+		readBtn.textContent = 'Read';
+		readBtn.classList.add('true');
 	} else {
-		readBtn.textContent = "Not Read";
+		readBtn.textContent = 'Not Read';
 	}
 	container.appendChild(readBtn);
 
-	const removeBtn = document.createElement("div");
-	removeBtn.classList.add("remove");
-	removeBtn.textContent = "Remove";
-	removeBtn.addEventListener("click", removeCard);
+	const removeBtn = document.createElement('div');
+	removeBtn.classList.add('remove');
+	removeBtn.textContent = 'Remove';
+	removeBtn.addEventListener('click', removeCard);
 	container.appendChild(removeBtn);
 
 	return container;
@@ -85,10 +84,10 @@ function createBookObject(event) {
 	);
 
 	if (isInLibrary(newBook)) {
-		errorMsg.classList.add("true");
+		errorMsg.classList.add('true');
 		return;
 	} else {
-		errorMsg.classList.remove("true");
+		errorMsg.classList.remove('true');
 		toggleForm();
 	}
 
@@ -99,27 +98,27 @@ function createBookObject(event) {
 function toggleReadBtn(event) {
 	const readBtn = event.target;
 
-	if (readBtn.classList.contains("true")) {
-		readBtn.classList.toggle("true");
-		readBtn.innerText = "Not Read";
+	if (readBtn.classList.contains('true')) {
+		readBtn.classList.toggle('true');
+		readBtn.innerText = 'Not Read';
 	} else {
-		readBtn.classList.toggle("true");
-		readBtn.innerText = "Read";
+		readBtn.classList.toggle('true');
+		readBtn.innerText = 'Read';
 	}
 }
 
 function removeCard(event) {
 	currentCard = event.target.parentNode;
-	const bookTitle = currentCard.querySelector(
-		":scope > .book-title"
-	).textContent;
+	const bookTitle = currentCard.querySelector(':scope > .book-title').textContent;
 	library.splice(library.findIndex((book) => book.title === bookTitle));
 	cardsContainer.removeChild(currentCard);
 }
 
 function toggleForm() {
-	formModal.classList.toggle("active");
+	formModal.classList.toggle('active');
+	overlay.classList.toggle('active');
 }
 
-bookForm.addEventListener("submit", createBookObject);
-addBookBtn.addEventListener("click", toggleForm);
+bookForm.addEventListener('submit', createBookObject);
+addBookBtn.addEventListener('click', toggleForm);
+overlay.addEventListener('click', toggleForm);
